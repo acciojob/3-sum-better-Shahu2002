acb7150b4ca17788ecc7346f3421df79c94f3893
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-09-17 00:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function threeSumClosest(nums, target) {
-    nums.sort((a, b) => a - b); // Sort the array in ascending order
-    let closestSum = Infinity;
+  
 
     for (let i = 0; i < nums.length - 2; i++) {
         let left = i + 1;
