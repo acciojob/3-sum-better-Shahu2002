2d056e54bf39c9df7e0947cfbbc6051d4973f885
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-10-26 16:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-function threeSumClosest(nums, target) {
-	let size = nums.length;
-	for(let i=0; i<size-2)
-		{
-			for(let j=1; j<size-1; j++){
-				for(let k=2; k<size; k++){
-					if(k===target){
-						return i+j+k;
-					}
-				}
-			}
-		}
+function threeSum(arr, target) {
+// write your code here
+	arr.sort((a, b) => a - b);
+  
+  // Initialize variables
+  let closestSum = arr[0] + arr[1] + arr[2];
+  
+  // Iterate through the array
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+    
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      
+      // Check if the current sum is closer to the target
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // Found an exact match, so return the sum
+        return sum;
+      }
+    }
+  }
+  
+  return closestSum;
+  
 }
-
+ 
+module.exports = threeSum;
